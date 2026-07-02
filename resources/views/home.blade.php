eb650415b646cbb07f11c181071f2ad2903b5155
sitirifazahra7-del <sitirifazahra7@gmail.com>
2026-07-02 21:07:15 +0700

    Restore Beli text on mobile and Pesan Sekarang on desktop

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -160,7 +160,8 @@
                         <button
                             onclick="openOrderModal({{ $product->id }}, '{{ addslashes($product->name) }}', {{ $product->price }}, '{{ $product->formatted_price }}')"
                             class="btn-gold w-full !py-2 md:!py-3.5 text-[10px] md:text-[11px] shadow-lg shadow-[#d4af37]/20 !px-1 md:!px-8">
-                            <span>✦ Beli </span>
+                            <span class="hidden md:inline">✦ Pesan Sekarang</span>
+                            <span class="md:hidden">Beli</span>
                         </button>
                     </div>
                 </div>
